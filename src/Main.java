9853cb788f968ea049a80d9bf15dbf01af94c5cd
Ivan <ivanengels1995@gmail.com>
2021-10-24 18:46:01 +0200

    4

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,21 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
-
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        System.out.println(a & b);
+        System.out.println(a & c);
+        System.out.println(!a | b);
+        System.out.println(a | c);
+        System.out.println(!a ^ b);
+        System.out.println(a ^ c);
+        boolean x = true;
+        boolean y = false;
+        int z = 25;
+        int q = 2;
+        System.out.println((x|y) | (z < 100) & !(true)^(q == 5));
+        boolean h = true, j = true, k = false;
+        System.out.println(h | j & k);
     }
 }
