9c40831b3be73ac6a89409a64682c7ac1c181431
Ivan <ivanengels1995@gmail.com>
2021-10-24 19:09:25 +0200

    6

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,11 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
-
+       int[] tablica = new int[5];
+        for (int i = 0; i < 5; i++)
+            tablica [(int) i] = i +1;
+        int zmienna = tablica[3];
+        for (int i = 0; i < 5; i++)
+            System.out.println("Kolejna komórka to: " + tablica[i]);
     }
 }
