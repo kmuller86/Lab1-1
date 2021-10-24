a758f94e20ea593b418c3b1e63685bbb4f62da0d
Ivan <ivanengels1995@gmail.com>
2021-10-24 15:27:25 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+           System.out.println("Hello world");
+           System.out.println("Życzę" + " " + "wam " + "miłej nauki!");
+           System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
 
     }
 }
